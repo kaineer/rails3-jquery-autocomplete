31f337e8b417cfa923decfbe9b424484f0a6f996
Konstantin Shabanov <etehtsea@gmail.com>
2011-05-25 18:00:57 +0700

    formtastic input fix

--- lib/rails3-jquery-autocomplete/formtastic_plugin.rb
+++ lib/rails3-jquery-autocomplete/formtastic_plugin.rb
@@ -36,7 +36,7 @@ module Rails3JQueryAutocomplete
 
     protected
     def autocompleted_string_input(method, options)
-      self.label(method, options_for_label(options)) << autocomplete_field(method, options.delete(:url), options)
+      self.label(method, options.delete(:label_html)) << autocomplete_field(method, options.delete(:url), options.delete(:input_html))
     end
   end
 end
